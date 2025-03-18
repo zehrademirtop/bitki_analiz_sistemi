81e7c39e2d40437f7087dd02a68631c0b589857a
zehrademirtop <zehrademirtop08@gmail.com>
2025-03-18 09:16:01 +0300

    eklenti

--- bitki_analiz_sistemi/Form1.Designer.cs
+++ bitki_analiz_sistemi/Form1.Designer.cs
@@ -28,13 +28,330 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBoxcap = new System.Windows.Forms.ComboBox();
+            this.comboBoxTuyDurumu = new System.Windows.Forms.ComboBox();
+            this.comboBoxDurus = new System.Windows.Forms.ComboBox();
+            this.comboBoxYuzey = new System.Windows.Forms.ComboBox();
+            this.comboBoxRenk = new System.Windows.Forms.ComboBox();
+            this.comboBoxDallanma = new System.Windows.Forms.ComboBox();
+            this.comboBoxNodyum = new System.Windows.Forms.ComboBox();
+            this.comboBoxUzunluk = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.TürAra = new System.Windows.Forms.Button();
+            this.TürAdı = new System.Windows.Forms.Label();
+            this.Bilgiver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.YellowGreen;
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(365, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gövde(Stem)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.YellowGreen;
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Çap";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.YellowGreen;
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(200, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tüy Durumu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.YellowGreen;
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(464, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Duruş";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.YellowGreen;
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(682, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Yüzey";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.YellowGreen;
+            this.label6.ForeColor = System.Drawing.Color.Maroon;
+            this.label6.Location = new System.Drawing.Point(5, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Renk";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.YellowGreen;
+            this.label7.ForeColor = System.Drawing.Color.Maroon;
+            this.label7.Location = new System.Drawing.Point(200, 131);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Dallanma";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.YellowGreen;
+            this.label8.ForeColor = System.Drawing.Color.Maroon;
+            this.label8.Location = new System.Drawing.Point(448, 138);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Nodyum";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.YellowGreen;
+            this.label9.ForeColor = System.Drawing.Color.Maroon;
+            this.label9.Location = new System.Drawing.Point(672, 138);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 16);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Uzunluk";
+            // 
+            // comboBoxcap
+            // 
+            this.comboBoxcap.FormattingEnabled = true;
+            this.comboBoxcap.Location = new System.Drawing.Point(50, 58);
+            this.comboBoxcap.Name = "comboBoxcap";
+            this.comboBoxcap.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxcap.TabIndex = 18;
+            // 
+            // comboBoxTuyDurumu
+            // 
+            this.comboBoxTuyDurumu.FormattingEnabled = true;
+            this.comboBoxTuyDurumu.Location = new System.Drawing.Point(285, 61);
+            this.comboBoxTuyDurumu.Name = "comboBoxTuyDurumu";
+            this.comboBoxTuyDurumu.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxTuyDurumu.TabIndex = 19;
+            // 
+            // comboBoxDurus
+            // 
+            this.comboBoxDurus.FormattingEnabled = true;
+            this.comboBoxDurus.Location = new System.Drawing.Point(512, 61);
+            this.comboBoxDurus.Name = "comboBoxDurus";
+            this.comboBoxDurus.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxDurus.TabIndex = 20;
+            // 
+            // comboBoxYuzey
+            // 
+            this.comboBoxYuzey.FormattingEnabled = true;
+            this.comboBoxYuzey.Location = new System.Drawing.Point(732, 64);
+            this.comboBoxYuzey.Name = "comboBoxYuzey";
+            this.comboBoxYuzey.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxYuzey.TabIndex = 21;
+            // 
+            // comboBoxRenk
+            // 
+            this.comboBoxRenk.FormattingEnabled = true;
+            this.comboBoxRenk.Location = new System.Drawing.Point(50, 123);
+            this.comboBoxRenk.Name = "comboBoxRenk";
+            this.comboBoxRenk.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxRenk.TabIndex = 22;
+            // 
+            // comboBoxDallanma
+            // 
+            this.comboBoxDallanma.FormattingEnabled = true;
+            this.comboBoxDallanma.Location = new System.Drawing.Point(285, 128);
+            this.comboBoxDallanma.Name = "comboBoxDallanma";
+            this.comboBoxDallanma.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxDallanma.TabIndex = 23;
+            // 
+            // comboBoxNodyum
+            // 
+            this.comboBoxNodyum.FormattingEnabled = true;
+            this.comboBoxNodyum.Location = new System.Drawing.Point(512, 130);
+            this.comboBoxNodyum.Name = "comboBoxNodyum";
+            this.comboBoxNodyum.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxNodyum.TabIndex = 24;
+            // 
+            // comboBoxUzunluk
+            // 
+            this.comboBoxUzunluk.FormattingEnabled = true;
+            this.comboBoxUzunluk.Location = new System.Drawing.Point(732, 128);
+            this.comboBoxUzunluk.Name = "comboBoxUzunluk";
+            this.comboBoxUzunluk.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxUzunluk.TabIndex = 25;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.YellowGreen;
+            this.button1.Location = new System.Drawing.Point(15, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "BtnEkle";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.YellowGreen;
+            this.button2.Location = new System.Drawing.Point(130, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 39);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "BtnGüncelle";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.YellowGreen;
+            this.button3.Location = new System.Drawing.Point(130, 303);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 42);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "BtnKaydet";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.YellowGreen;
+            this.button4.Location = new System.Drawing.Point(15, 303);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 42);
+            this.button4.TabIndex = 29;
+            this.button4.Text = "BtnSil";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // TürAra
+            // 
+            this.TürAra.BackColor = System.Drawing.Color.YellowGreen;
+            this.TürAra.Location = new System.Drawing.Point(391, 271);
+            this.TürAra.Name = "TürAra";
+            this.TürAra.Size = new System.Drawing.Size(177, 44);
+            this.TürAra.TabIndex = 30;
+            this.TürAra.Text = "TürAra";
+            this.TürAra.UseVisualStyleBackColor = false;
+           
+            // 
+            // TürAdı
+            // 
+            this.TürAdı.AutoSize = true;
+            this.TürAdı.BackColor = System.Drawing.Color.YellowGreen;
+            this.TürAdı.Location = new System.Drawing.Point(586, 285);
+            this.TürAdı.Name = "TürAdı";
+            this.TürAdı.Size = new System.Drawing.Size(47, 16);
+            this.TürAdı.TabIndex = 31;
+            this.TürAdı.Text = "TürAdı";
+            // 
+            // Bilgiver
+            // 
+            this.Bilgiver.BackColor = System.Drawing.Color.YellowGreen;
+            this.Bilgiver.Location = new System.Drawing.Point(391, 353);
+            this.Bilgiver.Name = "Bilgiver";
+            this.Bilgiver.Size = new System.Drawing.Size(177, 48);
+            this.Bilgiver.TabIndex = 32;
+            this.Bilgiver.Text = "Bilgiver";
+            this.Bilgiver.UseVisualStyleBackColor = false;
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(865, 486);
+            this.Controls.Add(this.Bilgiver);
+            this.Controls.Add(this.TürAdı);
+            this.Controls.Add(this.TürAra);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxUzunluk);
+            this.Controls.Add(this.comboBoxNodyum);
+            this.Controls.Add(this.comboBoxDallanma);
+            this.Controls.Add(this.comboBoxRenk);
+            this.Controls.Add(this.comboBoxYuzey);
+            this.Controls.Add(this.comboBoxDurus);
+            this.Controls.Add(this.comboBoxTuyDurumu);
+            this.Controls.Add(this.comboBoxcap);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBoxcap;
+        private System.Windows.Forms.ComboBox comboBoxTuyDurumu;
+        private System.Windows.Forms.ComboBox comboBoxDurus;
+        private System.Windows.Forms.ComboBox comboBoxYuzey;
+        private System.Windows.Forms.ComboBox comboBoxRenk;
+        private System.Windows.Forms.ComboBox comboBoxDallanma;
+        private System.Windows.Forms.ComboBox comboBoxNodyum;
+        private System.Windows.Forms.ComboBox comboBoxUzunluk;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button TürAra;
+        private System.Windows.Forms.Label TürAdı;
+        private System.Windows.Forms.Button Bilgiver;
     }
 }
 
--- bitki_analiz_sistemi/Form1.cs
+++ bitki_analiz_sistemi/Form1.cs
@@ -16,5 +16,10 @@ namespace bitki_analiz_sistemi
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
